0e06d712e6dc852c9b5827351af5e394f2cc5ee0
snailcookierun <snail.cookierun.kingdom@gmail.com>
2022-08-15 20:54:09 +0900

    Refactor Bosses and Commands class and add boss commands

--- Bots/GuildBot/main.ts
+++ Bots/GuildBot/main.ts
@@ -12,19 +12,27 @@ const isDuplicateExist = arr => new Set(arr).size !== arr.length;
 
 /* Global constants and data structures */
 /* enum BOSS_TYPE */
+/* Boss가 추가될 경우 BOSS_TYPE과 bossList에 추가해야 함 */
 enum BOSS_TYPE {
   DRAGON = "용",
   ANGEL = "천사",
   LICORICE = "감초"
 }
 
+
 /**
- * type Log - boss:(Boss), level:(int), damage:(int)
+ * class Log - boss:(BOSS_TYPE), level:(int), damage:(int)
  */
-type Log = {
+class Log {
   boss: BOSS_TYPE;
   level: number;
   damage: number;
+
+  constructor(boss: BOSS_TYPE, level: number, damage: number) {
+    this.boss = boss;
+    this.level = level;
+    this.damage = damage;
+  }
 }
 
 /**
@@ -46,24 +54,26 @@ class User {
 }
 
 class _Users {
-  userList : Array<User>;
+  userList: Array<User>;
 
   constructor() {
     this.userList = [];
   }
 
-  printTickets() : string {
+  /* printTickets: print remained tickets of users */
+  printTickets(): string {
     var text = "";
     var i = 0;
     var last = this.userList.length - 1;
     for (i = 0; i < last; i++) {
       text += this.userList[i].name + ": " + this.userList[i].tickets + "\n";
-  }
+    }
     text += this.userList[i].name + ": " + this.userList[i].tickets;
     return text;
   }
 
-  isNameValid(name:string) : boolean {
+  /* isNameValid: check name is valid */
+  isNameValid(name: string): boolean {
     if (isNumber(name)) {
       return false;
     }
@@ -72,24 +82,28 @@ class _Users {
     this.userList.forEach(u => username.push(u.name));
     return !username.includes(name);
   }
-  push(user:User) {
+
+  /* push: add user */
+  push(user: User) {
     this.userList.push(user);
   }
 }
 const Users = new _Users;
 
 /**
- * class Boss - type(BOSS_TYPE): {name:(BOSS_NAME), hps:(array<int>), curLevel:(int), curHp:(int), curUsers:(array<string>)}
- * e.g., var dragonName = Bosses[BOSS_TYPE.DRAGON].name;
+ * class Boss - type(BOSS_TYPE), name:(Array<string>), hps:(array<int>), curLevel:(int), curHp:(int), curUsers:(array<string>)}
  */
 class Boss {
-  name: BOSS_TYPE;
+  type: BOSS_TYPE;
+  name: Array<string>;
   hps: Array<number>;
   curLevel: number;
   curHp: number;
   curUsers: Array<User>;
 
-  constructor(name: BOSS_TYPE) {
+  // constructor - type:BOSS_TYPE, name:boss's nickname
+  constructor(type: BOSS_TYPE, name: Array<string>) {
+    this.type = type;
     this.name = name;
     this.hps = [];
     this.curLevel = 1;
@@ -98,87 +112,118 @@ class Boss {
   }
 }
 
-const Bosses: { [key in BOSS_TYPE]: Boss } = {
-  [BOSS_TYPE.DRAGON]: new Boss(BOSS_TYPE.DRAGON),
-  [BOSS_TYPE.ANGEL]: new Boss(BOSS_TYPE.ANGEL),
-  [BOSS_TYPE.LICORICE]: new Boss(BOSS_TYPE.LICORICE)
+const bossList: { [key in BOSS_TYPE]: Boss } = {
+  [BOSS_TYPE.DRAGON]: new Boss(BOSS_TYPE.DRAGON, ["용", "레벨", "드래곤", "ㅇ", "ㄼ", "ㄹㅂ"]),
+  [BOSS_TYPE.ANGEL]: new Boss(BOSS_TYPE.ANGEL, ["천사", "천", "대천사", "ㅊㅅ", "ㅊ"]),
+  [BOSS_TYPE.LICORICE]: new Boss(BOSS_TYPE.LICORICE, ["감초", "감", "ㄱㅊ", "ㄱ"])
 };
+const rBossList: { [key: string]: BOSS_TYPE } = Object.keys(bossList).reduce((acc, propName) =>
+  bossList[propName].name.reduce((a, name) => { a[name] = propName; return a; }, acc), {});
 
 
+class _Bosses {
+  bossList: { [key in BOSS_TYPE]: Boss };
+  rBossList: { [key: string]: BOSS_TYPE };
 
+  constructor(bossList: { [key in BOSS_TYPE]: Boss }, rBossList: { [key: string]: BOSS_TYPE }) {
+    this.bossList = bossList;
+    this.rBossList = rBossList;
+  }
 
-/**
- * addUser: add user
- * Users struct: {name:(string), tickets:(int), log:(array<damage_struct>)}
- * (string) name: user's name
- * (int) tickets: current remained tickets
- * (array<damage_struct>) log: damage log for each boss/level/damage
- * damage_struct should be {boss:(string), level:(int), damage:(int)}
-*/
-function addUser(commands: Array<string>) : string {
-  if (commands.length == 2) { // /유저추가 이름
-    if (!Users.isNameValid(commands[1])) {
-      return "유효하지 않는 닉네임입니다.";
-    }
-    Users.push(new User(commands[1],0));
-    return Users.printTickets();
-  } else if (commands.length == 3 && isUnsigned(commands[2]) && Number(commands[2]) <= 9) { // /유저추가 이름 티켓수
-    if (!Users.isNameValid(commands[1])) {
-      return "유효하지 않는 닉네임입니다.";
-    }
-    Users.push(new User(commands[1],Number(commands[2])));
-    return Users.printTickets();
-  } else if (commands.length >= 3) { // /유저추가 이름1 이름2
-    commands.shift();
-    if (commands.every(u => Users.isNameValid(u))) {
-      if (isDuplicateExist(commands)) {
-        return "중복 닉네임이 있습니다.";
+  isNameExist(str: string): boolean {
+    return rBossList.hasOwnProperty(str);
+  }
+
+  find(str: string): Boss {
+    return this.bossList[this.rBossList[str]];
+  }
+
+}
+const Bosses = new _Bosses(bossList, rBossList);
+
+class _Commands {
+
+  /**
+   * addUser: add user
+   * Users struct: {name:(string), tickets:(int), log:(array<damage_struct>)}
+   * (string) name: user's name
+   * (int) tickets: current remained tickets
+   * (array<damage_struct>) log: damage log for each boss/level/damage
+   * damage_struct should be {boss:(string), level:(int), damage:(int)}
+  */
+  addUser(commands: Array<string>): string {
+    if (commands.length == 2) { // /유저추가 이름
+      if (!Users.isNameValid(commands[1])) {
+        return "유효하지 않는 닉네임입니다.";
       }
-      commands.forEach(u => Users.push(new User(u,0)));
+      Users.push(new User(commands[1], 0));
       return Users.printTickets();
+    } else if (commands.length == 3 && isUnsigned(commands[2]) && Number(commands[2]) <= 9) { // /유저추가 이름 티켓수
+      if (!Users.isNameValid(commands[1])) {
+        return "유효하지 않는 닉네임입니다.";
+      }
+      Users.push(new User(commands[1], Number(commands[2])));
+      return Users.printTickets();
+    } else if (commands.length >= 3) { // /유저추가 이름1 이름2
+      commands.shift();
+      if (commands.every(u => Users.isNameValid(u))) {
+        if (isDuplicateExist(commands)) {
+          return "중복 닉네임이 있습니다.";
+        }
+        commands.forEach(u => Users.push(new User(u, 0)));
+        return Users.printTickets();
+      } else {
+        return "유효하지 않는 닉네임입니다.";
+      }
     } else {
-      return "유효하지 않는 닉네임입니다.";
+      return "명령어 오입력\n- /유저추가 이름\n- /유저추가 이름 티켓수(0~9)\n- /유저추가 이름1 이름2";
     }
-  } else {
-    return "명령어 오입력\n- /유저추가 이름\n- /유저추가 이름 티켓수(0~9)\n- /유저추가 이름1 이름2";
+  }
+
+  addDamage(commands: Array<string>): string {
+    return "";
+  }
+
+  deleteDamage(commands: Array<string>): string {
+    return "";
+  }
+
+  moveBossLevel(commands: Array<string>): string {
+    return "";
+  }
+
+  setBossLevel(commands: Array<string>): string {
+    if(commands.length == 3 && !isNumber(commands[1]) && isNumber(commands[2])){
+      if(!Bosses.isNameExist(commands[1])) {
+        return "없는 보스명입니다.\n - 보스명: " + Object.keys(rBossList).join(" ");
+      }
+      return "있다";
+    } else {
+      return "명령어 오입력\n- /보스셋팅 보스명 단계(1~n)";
+    }
   }
 }
-
-function addDamage(commands:Array<string>) :string {
-  return "";
-}
-
-function deleteDamage(commands:Array<string>) :string {
-  return "";
-}
-
-function moveBossLevel(commands:Array<string>) :string {
-  return "";
-}
-
-function setBossLevel(commands:Array<string>) :string {
-  return "";
-}
+const Commands = new _Commands();
 
 /**
  * processCommand: Process command with parsing msg
  * trim multi-whitespace cases and map to specific commands
  */
-function processCommand(msg:string) : string {
+function processCommand(msg: string): string {
   var commands = msg.trim().split(/\s+/);
   switch (commands[0]) {
     default: return "알 수 없는 명령어입니다.\n- /명령어"; break;
-    case '/유저추가': return addUser(commands); break;
+    case '/유저추가': return Commands.addUser(commands); break;
     case '/딜':
     case '/딜량':
     case '/ㄷ':
-    case '/ㄷㄹ': return addDamage(commands); break;
+    case '/ㄷㄹ': return Commands.addDamage(commands); break;
     case '/딜오타':
-    case '/ㄷㅇㅌ': return deleteDamage(commands); break;
+    case '/ㄷㅇㅌ': return Commands.deleteDamage(commands); break;
     case '/이달':
     case '/이어달리기': //TODO
-    case '/컷': return moveBossLevel(commands); break;
-    case '/보스셋팅': return setBossLevel(commands); break;
+    case '/컷': return Commands.moveBossLevel(commands); break;
+    case '/보스셋팅': return Commands.setBossLevel(commands); break;
   }
 }
 
