86251e7f79b08f18c237efa13b73082b562e79db
snailcookierun <snail.cookierun.kingdom@gmail.com>
2023-05-05 20:04:35 +0900

    Save refresh token to file

--- node/modules/cafe.ts
+++ node/modules/cafe.ts
@@ -1,5 +1,6 @@
 var express = require('express');
 var axios = require('axios');
+var fs = require('fs');
 var crypto: Crypto = require('crypto');
 
 class _Cafe {
@@ -30,36 +31,53 @@ class _Cafe {
     this.content = encodeURI("테스트 글입니다.");
   }
 
-  refreshToken(msg, verbose:boolean, successCallback) {
+  async saveTokenToFile() {
+    var payload = JSON.stringify({refresh_token: this.refresh_token});
+    await fs.writeFile(this.config.cafe.token_path, payload, (e) => {if(e){Logs.e(e,false)}});
+  }
+
+  async loadSavedTokenIfExist():Promise<string> {
+    try {
+      const content = await fs.readFile(this.config.cafe.token_path);
+      const credentials = JSON.parse(content);
+      return credentials.refresh_token;
+    } catch (err) {
+      return "";
+    }
+  }
+
+  async refreshToken(msg, verbose:boolean, successCallback:Function) {
+    if (this.refresh_token == "") {
+      this.refresh_token = await this.loadSavedTokenIfExist();
+    }
     var api_url = 'https://nid.naver.com/oauth2.0/token?grant_type=refresh_token&'
       + "client_id=" + this.client_id + '&client_secret=' + this.client_secret + '&refresh_token=' + this.refresh_token;
     axios.get(api_url).then(function (response) {
-      console.log(response.data);
       if (response.data.error != null) {
         if(verbose) { msg.reply("리프레시에 실패하였습니다."); }
       } else {
         Cafe.access_token = response.data.access_token;
         Cafe.refresh_token = response.data.refresh_token;
+        Cafe.saveTokenToFile();
         if(verbose) { msg.reply("리프레시에 성공하였습니다."); }
       }
       successCallback(msg);
     }).catch(function (error) {
-      console.log(error);
+      Logs.e(error,false);
       msg.reply("리프레시에 실패하였습니다.");
     })
   }
 
-  writeCafePost(msg, verbose) {
+  writeCafePost(msg, verbose:boolean) {
     var header = "Bearer " + this.access_token;
     var api_url = 'https://openapi.naver.com/v1/cafe/' + this.clubid + '/menu/' + this.menuid + '/articles';
     var form = new FormData();
     form.append('subject', this.subject);
     form.append('content', this.content);
     axios.post(api_url, form, { headers: { 'Authorization': header } }).then(function (response) {
-      console.log(response.data);
       if(verbose) { msg.reply("포스트에 성공하였습니다.\n" + response.data.message.result.articleUrl); }
     }).catch(function (error) {
-      console.log(error);
+      Logs.e(error, false);
       msg.reply("포스트에 실패하였습니다.");
     });
   }
@@ -80,19 +98,19 @@ Cafe.app.get('/callback', function (req, res) {
   var api_url = 'https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id='
     + Cafe.client_id + '&client_secret=' + Cafe.client_secret + '&redirect_uri=' + Cafe.redirectURI + '&code=' + code + '&state=' + state;
   axios.get(api_url, { headers: { 'X-Naver-Client-Id': Cafe.client_id, 'X-Naver-Client-Secret': Cafe.client_secret } }).then(function (response) {
-    console.log(response.data);
     Cafe.access_token = response.data.access_token;
     Cafe.refresh_token = response.data.refresh_token;
+    Cafe.saveTokenToFile();
     res.writeHead(200, { 'Content-Type': 'text/json;charset=utf-8' });
-    res.end("로그인 성공");
+    res.end("로그인에 성공하였습니다.");
   }).catch(function (error) {
-    console.log(error);
+    Logs.e(error, false);
   })
 });
 
 
 Cafe.app.listen(Cafe.config.cafe.port, function () {
-  console.log(Cafe.config.cafe.host + ':' + Cafe.config.cafe.port + '/naverlogin listening on port 8888!');
+  Logs.d(Cafe.config.cafe.host + ':' + Cafe.config.cafe.port + '/naverlogin listening on port 8888!',false);
 });
 
 
